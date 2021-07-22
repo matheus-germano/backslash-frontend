b91a283c70f575fc1a36f9a5ce798d59ad6b6932
Matheus Germano da Costa <62772781+matheus-germano@users.noreply.github.com>
2021-07-22 16:53:50 -0300

    Update index.js

--- src/pages/Feed/index.js
+++ src/pages/Feed/index.js
@@ -1,12 +1,12 @@
 //  importing dependencies
-import { useState } from 'react'
-import { FaTimes, FaBars } from 'react-icons/fa'
+// import { useState } from 'react'
+// import { FaTimes, FaBars } from 'react-icons/fa'
 
 // importing components
 import { Menu } from "../../components/Menu";
 
 export function Feed() {
-  const [menuOpen, setMenuOpen] = useState(false);
+//   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
     <>
